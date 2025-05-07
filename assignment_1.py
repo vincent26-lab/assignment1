769c764d7c68723420f2f257635920b3820b5f62
vincent26-lab <vincentsunday700@yahoo.com>
2025-05-06 23:37:06 -0400

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,21 @@
+# Even or ODD###########333
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+    pass
+
+
+#####Convert a number to string#########################
+
+def number_to_string(num):
+    return str(num)
+    pass 
+
+########################Remove White space#################
+
+def no_space(x):
+    return x.replace(" ", "")
